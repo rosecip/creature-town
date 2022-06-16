1a62c446bc3ac15d967448f71baa6b595d96d6b7
rosecip <rosecipriano10@gmail.com>
2022-06-16 17:56:16 -0400

    created nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import { Container, Nav, NavDropdown } from "react-bootstrap";
+import Navbar from "react-bootstrap/navbar";
+
+const NavBar = (props) => {
+  return (
+    <Navbar sticky="top" className="nav-bar">
+      <Container>
+        <div className="nav-bar-contents">
+          <Navbar.Brand className="you-are-in-creature-town">
+            you are now in CREATURE TOWN...
+          </Navbar.Brand>
+          <div className="nav-bar-buttons">
+            <button className="button explore-button">EXPLORE</button>
+            <button className="button retreat-button">RETREAT</button>
+          </div>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
